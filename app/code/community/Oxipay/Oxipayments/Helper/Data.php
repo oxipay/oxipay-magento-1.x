42edb1017599fa8624ef8f6460b7c7eda898ee13
Siyu Qian <david@magebinary.com>
2017-11-23 15:45:02 +1300

    SP-650 Add Debug fields and output to Oxipay extension

--- app/code/community/Oxipay/Oxipayments/Helper/Data.php
+++ app/code/community/Oxipay/Oxipayments/Helper/Data.php
@@ -6,11 +6,7 @@
  * Provides helper methods for retrieving data for the oxipay plugin
  */
 class Oxipay_Oxipayments_Helper_Data extends Mage_Core_Helper_Abstract
-{   
-    public static function init()
-    {
-    }
-
+{
     /**
      * get the URL of the configured oxipay gateway checkout
      * @return string
@@ -32,5 +28,14 @@ class Oxipay_Oxipayments_Helper_Data extends Mage_Core_Helper_Abstract
     public static function getCancelledUrl($orderId) {
         return Mage::getBaseUrl() . "oxipayments/payment/cancel?orderId=$orderId";
     }
+
+    /**
+     * isDebugMode
+     * Get debug setting from backend
+     * @return boolean
+     */
+    public function isDebugMode()
+    {
+        return Mage::getStoreConfig('payment/oxipayments/debug');
+    }
 }
-Oxipay_Oxipayments_Helper_Data::init();
