0ecab4fc9109f0de9cecda89f233c66dea3b8077
Tao Wang <hellotaotao@gmail.com>
2018-10-05 14:45:40 +0930

    move the refund function from observer to Paymentmethod.php

--- app/code/community/Oxipay/Oxipayments/Model/Paymentmethod.php
+++ app/code/community/Oxipay/Oxipayments/Model/Paymentmethod.php
@@ -25,6 +25,75 @@ class Oxipay_Oxipayments_Model_Paymentmethod extends Mage_Payment_Model_Method_A
     {
         return Mage::getUrl('oxipayments/payment/start', array('_secure' => false));
     }
+
+    public function refund(Varien_Object $payment, $amount)
+    {
+        $url = 'https://portalssandbox.oxipay.com.au/api/ExternalRefund/processrefund';
+        $result = 'unknown';
+
+        $merchant_number = Mage::getStoreConfig('payment/oxipayments/merchant_number');
+        $apiKey = Mage::getStoreConfig('payment/oxipayments/api_key');
+
+        if (empty($payment->getData('creditmemo'))) {
+            return;
+        }
+        $refund_amount = $amount;
+        $transaction_id = $payment->getData()['creditmemo']->getData('invoice')->getData('transaction_id');
+        $refund_details = array(
+            "x_merchant_number" => $merchant_number,
+            "x_purchase_number" => $transaction_id,
+            "x_amount" => $refund_amount,
+            "x_reason" => "Test refund"
+        );
+
+        $refund_signature = Oxipay_Oxipayments_Helper_Crypto::generateSignature($refund_details, $apiKey);
+        $refund_details['signature'] = $refund_signature;
+
+        $json = json_encode($refund_details);
+
+        $context = stream_context_create(array(
+            'http' => array(
+                'method' => 'POST',
+                'header' =>
+                    'Content-Type: application/json',
+                'content' => $json
+            )
+        ));
+
+        $return_message = file_get_contents($url, null, $context);
+        $parsed = ($this->parseHeaders($http_response_header));
+
+        if ($parsed['response_code'] == '204') {
+            $result = 'success';
+        } elseif ($parsed['response_code'] == '401') {
+            $result = "Failed Signature Check";
+        } elseif ($parsed['response_code'] == '400') {
+            $result = $return_message;
+        }
+//        return 'Failed Signature Check';
+        return $this;
+
+//        $event = $observer->getEvent();
+//        $creditmemo_object = $observer["creditmemo"];
+//        $refund_value = $creditmemo_object->getSubtotal();
+//        Mage::log('[oxipay][cron][cancelOxipayPendingOrders]Start', Zend_Log::DEBUG, self::LOG_FILE);
+    }
+
+    function parseHeaders($headers)
+    {
+        $head = array();
+        foreach ($headers as $k => $v) {
+            $t = explode(':', $v, 2);
+            if (isset($t[1]))
+                $head[trim($t[0])] = trim($t[1]);
+            else {
+                $head[] = $v;
+                if (preg_match("#HTTP/[0-9\.]+\s+([0-9]+)#", $v, $out))
+                    $head['response_code'] = intval($out[1]);
+            }
+        }
+        return $head;
+    }
 }
 
 ?>
